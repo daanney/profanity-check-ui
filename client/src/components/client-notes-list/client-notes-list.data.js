292819d0e1ecea4bdb95645cb257d7ff7c027161
danney <danney@macbook.local>
2020-05-25 11:29:20 +0200

    updated client note description as introduction

--- client/src/components/client-notes-list/client-notes-list.data.js
+++ client/src/components/client-notes-list/client-notes-list.data.js
@@ -5,6 +5,7 @@ export const NOTES_LIST = [
 		description: `This example client note is used as short description to this application. With this app, documents can be attached
 		to client notes (e.g. this one). Attempted document uploads are checked against malicious/inappropriate content on a backend server.
 		This is achieved by a simple list of banned words or phrases, which can be configured in the "Banned Words" tab. Note that with this app version, 
-		existing documents are not verified against newly configured words/phrases.`
+		existing documents are not verified against newly configured words/phrases. Uploaded files cannot be retrieved in this version, 
+		but the user is notified if they upload an invalid file with cases (empty, too large, invalid type or discarded by profanity check).`
 	}
 ]
